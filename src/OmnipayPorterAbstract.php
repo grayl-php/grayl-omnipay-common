0516a801cea8a2db279167b340f87d778790a0fc
Grogwood LLC <dev@grogwood.com>
2020-10-19 14:24:21 -0700

    Code formatting

--- src/OmnipayPorterAbstract.php
+++ src/OmnipayPorterAbstract.php
@@ -1,69 +1,64 @@
 <?php
 
-namespace Grayl\Omnipay\Common;
+   namespace Grayl\Omnipay\Common;
 
-use Grayl\Gateway\Common\GatewayPorterAbstract;
-use Grayl\Omnipay\Common\Entity\OmnipayGatewayCreditCard;
-use Grayl\Omnipay\Common\Entity\OmnipayGatewayDataAbstract;
+   use Grayl\Gateway\Common\GatewayPorterAbstract;
+   use Grayl\Omnipay\Common\Entity\OmnipayGatewayCreditCard;
+   use Grayl\Omnipay\Common\Entity\OmnipayGatewayDataAbstract;
 
-/**
- * Abstract class OmnipayPorterAbstract
- * The abstract class for all Omnipay API porters
- * @method OmnipayGatewayDataAbstract getSavedGatewayDataEntity (string $endpoint_id)
- *
- * @package Grayl\Omnipay\Common
- */
-abstract class OmnipayPorterAbstract extends
-    GatewayPorterAbstract implements
-    OmnipayPorterInterface
-{
+   /**
+    * Abstract class OmnipayPorterAbstract
+    * The abstract class for all Omnipay API porters
+    * @method OmnipayGatewayDataAbstract getSavedGatewayDataEntity ( string $endpoint_id )
+    *
+    * @package Grayl\Omnipay\Common
+    */
+   abstract class OmnipayPorterAbstract extends GatewayPorterAbstract implements OmnipayPorterInterface
+   {
 
-    /**
-     * Grabs any offsite URLs for the environment
-     *
-     * @return array
-     */
-    public function getOffsiteURLs(): array
-    {
+      /**
+       * Grabs any offsite URLs for the environment
+       *
+       * @return array
+       */
+      public function getOffsiteURLs (): array
+      {
 
-        // Get the array of offsite URLs from the config
-        $urls = $this->config->getConfig('offsite_urls');
+         // Get the array of offsite URLs from the config
+         $urls = $this->config->getConfig( 'offsite_urls' );
 
-        // Use environment specific URLs if they are set
-        if ( ! empty($urls[$this->environment])) {
+         // Use environment specific URLs if they are set
+         if ( ! empty( $urls[ $this->environment ] ) ) {
             // Return the environment URLs
-            return $urls[$this->environment];
-        }
+            return $urls[ $this->environment ];
+         }
 
-        // No URLs set for this environment
-        return [];
-    }
+         // No URLs set for this environment
+         return [];
+      }
 
 
-    /**
-     * Creates a new OmnipayGatewayCreditCard entity
-     *
-     * @param string $number The credit card number
-     * @param int    $month  The credit card expiry month (2 digit)
-     * @param int    $year   The credit card expiry year (4 digit)
-     * @param string $cvv    The credit card CVV
-     *
-     * @return OmnipayGatewayCreditCard
-     */
-    public function newOmnipayGatewayCreditCard(
-        string $number,
-        int $month,
-        int $year,
-        string $cvv
-    ): OmnipayGatewayCreditCard {
+      /**
+       * Creates a new OmnipayGatewayCreditCard entity
+       *
+       * @param string $number The credit card number
+       * @param int    $month  The credit card expiry month (2 digit)
+       * @param int    $year   The credit card expiry year (4 digit)
+       * @param string $cvv    The credit card CVV
+       *
+       * @return OmnipayGatewayCreditCard
+       */
+      public function newOmnipayGatewayCreditCard ( string $number,
+                                                    int $month,
+                                                    int $year,
+                                                    string $cvv ): OmnipayGatewayCreditCard
+      {
 
-        // Return the new entity
-        return new OmnipayGatewayCreditCard(
-            $number,
-            $month,
-            $year,
-            $cvv
-        );
-    }
+         // Return the new entity
+         return new OmnipayGatewayCreditCard( $number,
+                                              $month,
+                                              $year,
+                                              $cvv );
+      }
 
-}
+   }
--- src/Service/OmnipayResponseServiceAbstract.php
+++ src/Service/OmnipayResponseServiceAbstract.php
@@ -1,167 +1,166 @@
 <?php
 
-namespace Grayl\Omnipay\Common\Service;
+   namespace Grayl\Omnipay\Common\Service;
 
-use Grayl\Omnipay\Common\Entity\OmnipayResponseDataAbstract;
+   use Grayl\Omnipay\Common\Entity\OmnipayResponseDataAbstract;
 
-/**
- * Abstract class OmnipayResponseServiceAbstract
- * The abstract class for all Omnipay payment gateway response services
- *
- * @package Grayl\Omnipay\Common
- */
-abstract class OmnipayResponseServiceAbstract implements
-    OmnipayResponseServiceInterface
-{
+   /**
+    * Abstract class OmnipayResponseServiceAbstract
+    * The abstract class for all Omnipay payment gateway response services
+    *
+    * @package Grayl\Omnipay\Common
+    */
+   abstract class OmnipayResponseServiceAbstract implements OmnipayResponseServiceInterface
+   {
 
-    /**
-     * Returns a true / false value based on a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to check
-     *
-     * @return bool
-     */
-    public function isSuccessful($response_data): bool
-    {
+      /**
+       * Returns a true / false value based on a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to check
+       *
+       * @return bool
+       */
+      public function isSuccessful ( $response_data ): bool
+      {
 
-        // Return the success status from Omnipay
-        return $response_data->getApiResponse()
-            ->isSuccessful();
-    }
-
-
-    /**
-     * Returns the reference ID from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to pull the reference ID from
-     *
-     * @return string
-     */
-    public function getReferenceID($response_data): ?string
-    {
-
-        // Return the reference ID from Omnipay
-        return $response_data->getApiResponse()
-            ->getTransactionReference();
-    }
+         // Return the success status from Omnipay
+         return $response_data->getApiResponse()
+                              ->isSuccessful();
+      }
 
 
-    /**
-     * Returns the status message from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to get the message from
-     *
-     * @return string
-     */
-    public function getMessage($response_data): ?string
-    {
+      /**
+       * Returns the reference ID from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to pull the reference ID from
+       *
+       * @return string
+       */
+      public function getReferenceID ( $response_data ): ?string
+      {
 
-        // Return the message from Omnipay
-        return $response_data->getApiResponse()
-            ->getMessage();
-    }
-
-
-    /**
-     * Returns the raw data from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to pull the data from
-     *
-     * @return mixed
-     */
-    public function getData($response_data)
-    {
-
-        // Return the raw data from Omnipay
-        return $response_data->getApiResponse()
-            ->getData();
-    }
+         // Return the reference ID from Omnipay
+         return $response_data->getApiResponse()
+                              ->getTransactionReference();
+      }
 
 
-    /**
-     * Returns a true / false value if a payment is still pending from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to check
-     *
-     * @return bool
-     */
-    public function isPending($response_data): bool
-    {
+      /**
+       * Returns the status message from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to get the message from
+       *
+       * @return string
+       */
+      public function getMessage ( $response_data ): ?string
+      {
 
-        // Use the pending status from Omnipay
-        return $response_data->getApiResponse()
-            ->isPending();
-    }
-
-
-    /**
-     * Returns the AVS result code from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to pull the AVS code from
-     *
-     * @return string
-     */
-    public function getAVSCode($response_data): ?string
-    {
-
-        // Not used for all gateways
-        return null;
-    }
+         // Return the message from Omnipay
+         return $response_data->getApiResponse()
+                              ->getMessage();
+      }
 
 
-    /**
-     * Returns the CVV result code from a gateway API response
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to pull the CVV code from
-     *
-     * @return string
-     */
-    public function getCVVCode($response_data): ?string
-    {
+      /**
+       * Returns the raw data from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to pull the data from
+       *
+       * @return mixed
+       */
+      public function getData ( $response_data )
+      {
 
-        // Not used for all gateways
-        return null;
-    }
-
-
-    /**
-     * Checks if a gateway is requesting an offsite redirect for payment
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to check for redirection
-     *
-     * @return bool
-     */
-    public function isRedirect($response_data): bool
-    {
-
-        // Return the redirect status from Omnipay
-        return $response_data->getApiResponse()
-            ->isRedirect();
-    }
+         // Return the raw data from Omnipay
+         return $response_data->getApiResponse()
+                              ->getData();
+      }
 
 
-    /**
-     * Returns the URL for responses that require an offsite redirect for payment
-     *
-     * @param OmnipayResponseDataAbstract $response_data The response object to get the URL from
-     *
-     * @return string
-     */
-    public function getRedirectURL($response_data): ?string
-    {
+      /**
+       * Returns a true / false value if a payment is still pending from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to check
+       *
+       * @return bool
+       */
+      public function isPending ( $response_data ): bool
+      {
 
-        // Get the API response
-        $api_response = $response_data->getApiResponse();
+         // Use the pending status from Omnipay
+         return $response_data->getApiResponse()
+                              ->isPending();
+      }
 
-        // Make sure there is a redirect request
-        if ($api_response->isRedirect()) {
+
+      /**
+       * Returns the AVS result code from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to pull the AVS code from
+       *
+       * @return string
+       */
+      public function getAVSCode ( $response_data ): ?string
+      {
+
+         // Not used for all gateways
+         return null;
+      }
+
+
+      /**
+       * Returns the CVV result code from a gateway API response
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to pull the CVV code from
+       *
+       * @return string
+       */
+      public function getCVVCode ( $response_data ): ?string
+      {
+
+         // Not used for all gateways
+         return null;
+      }
+
+
+      /**
+       * Checks if a gateway is requesting an offsite redirect for payment
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to check for redirection
+       *
+       * @return bool
+       */
+      public function isRedirect ( $response_data ): bool
+      {
+
+         // Return the redirect status from Omnipay
+         return $response_data->getApiResponse()
+                              ->isRedirect();
+      }
+
+
+      /**
+       * Returns the URL for responses that require an offsite redirect for payment
+       *
+       * @param OmnipayResponseDataAbstract $response_data The response object to get the URL from
+       *
+       * @return string
+       */
+      public function getRedirectURL ( $response_data ): ?string
+      {
+
+         // Get the API response
+         $api_response = $response_data->getApiResponse();
+
+         // Make sure there is a redirect request
+         if ( $api_response->isRedirect() ) {
             // Return the redirect status from Omnipay
             return $api_response->getRedirectResponse()
-                ->getTargetUrl();
-        }
+                                ->getTargetUrl();
+         }
 
-        // No redirect
-        return null;
-    }
+         // No redirect
+         return null;
+      }
 
-}
+   }
